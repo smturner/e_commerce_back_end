f64410d028b7ada643af459ec36f290a91222836
Sarah Turner <turnersarah228@gmail.com>
2022-04-27 20:54:57 -0500

    added create route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,7 +35,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+  try{
+    const categoryData = await Category.create( {
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData)
+  }catch (err) {
+    res.status(400).json(err);
+  }
   // create a new category
 });
 
